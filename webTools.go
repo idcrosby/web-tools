c992c0e0f0951ece92dcb667ba521df9c0955bd6
unknown <x078259@KLM124989.cs.ad.klmcorp.net>
2014-08-16 09:03:34 +0200

    Initial commit of basic tools

---
+++ webTools.go
@@ -0,0 +1,60 @@
+package myTools
+
+import (
+	"crypto/md5"
+	"crypto/sha1"
+	"encoding/base64"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"time"
+)
+
+func Base64Encode(encode []byte) string {
+	fmt.Println("encoding data...")
+	str := base64.StdEncoding.EncodeToString(encode)
+	return str
+}
+
+func Base64Decode(decode string) []byte {
+	data, err := base64.StdEncoding.DecodeString(decode)
+	if err != nil {
+		fmt.Println("error:", err)
+		return nil
+	}
+	return data
+}
+
+func ValidateJson(bytes []byte) (buf []byte) {
+	var f interface{}
+	err := json.Unmarshal(bytes, &f)
+	if err != nil {
+		fmt.Println("Error reading Json.")
+		return nil
+	}
+	buf, err = json.MarshalIndent(&f, "", "   ")
+	check(err)
+	return
+}
+
+func Md5Hash(data []byte) string {
+	h := md5.New()
+	h.Write(data)
+	return hex.EncodeToString(h.Sum(nil))
+}
+
+func Sha1Hash(data []byte) string {
+	h := sha1.New()
+	h.Write(data)
+	return hex.EncodeToString(h.Sum(nil))
+}
+
+func ConvertTimeFromEpoch(epoch int64) time.Time {
+	return time.Unix(epoch, 0)
+}
+
+func ConvertTimeToEpoch(convert time.Time) int64 {
+	return convert.Unix()
+}
+
+func check(err error) { if err != nil { panic(err) } }
---
+++ webTools_test.go
@@ -0,0 +1,74 @@
+package myTools
+
+import (
+	"testing"
+	"fmt"
+	"time"
+)
+
+func TestBase64Encode(t *testing.T) {
+	fmt.Println("testing encode")
+	str := "JEBtcGxlICsgZGF0YSZ0b0VuYzBkZQ=="
+	data := []byte("$@mple + data&toEnc0de")
+	if x:= Base64Encode(data); x != str  {
+		t.Errorf("Base64Encode(" + string(data) + ") = " + x + ", want " + str)
+	}	
+}
+
+func TestBase64Decode(t *testing.T) {
+	str := "JEBtcGxlICsgZGF0YSZ0b0VuYzBkZQ=="
+	data := []byte("$@mple + data&toEnc0de")
+	if x := Base64Decode(str); string(x) != string(data) {
+		t.Errorf("Base64Decode(" + str + ") = " + string(x) + ", want " + string(data))
+	}
+}
+
+func TestValidateJsonValid(t *testing.T) {
+	validJson := []byte("{\"sample\":\"data\"}")
+	if x := ValidateJson(validJson); x == nil {
+		t.Errorf("ValidateJson(" + string(validJson) + ") = nil")
+	}
+}
+
+func TestValidateJsonInValid(t *testing.T) {
+	invalidJson := []byte("{\"sample\":\"data\",\"missing\"}")
+	if x := ValidateJson(invalidJson); x != nil {
+		t.Errorf("ValidateJson(" + string(invalidJson) + ") = " + string(x) + ", want nil")
+	}
+}
+
+func TestMd5Hash(t *testing.T) {
+	data := []byte("h@$H_th1s *tr1ng")
+	hash := "7952b401cd8a52dc40536d7e1b4c6658"
+	if x := Md5Hash(data); x != hash {
+		t.Errorf("Md5Hash(" + string(data) + ") = " + x + ", want " + hash)
+	}
+}
+
+func TestSha1Hash(t *testing.T) {
+	data := []byte("h@$H_th1s *tr1ng")
+	hash := "832ed14e88a3e412ebf3474c83ce01cf99c90b87"
+	if x := Sha1Hash(data); x != hash {
+		t.Errorf("Sha1Hash(" + string(data) + ") = " + x + ", want " + hash)
+	}
+}
+
+func TestConvertTimeToEpoch(t *testing.T) {
+	var epoch int64
+	epoch = 1408109234
+	timeString := "2014-08-15 15:27:14 +0200 CEST"
+	myTime, _ := time.Parse("2006-01-02 15:04:05 -0700 MST", timeString)
+	if x := ConvertTimeToEpoch(myTime); x != epoch {
+		t.Errorf("ConvertTimeToEpoch(" + myTime.String() + ")=%v, want %v", x, epoch)
+	}
+}
+
+func TestConvertTimeFromEpoch(t *testing.T) {
+	var epoch int64
+	epoch = 1408109234
+	timeString := "2014-08-15 15:27:14 +0200 CEST"
+	if x := ConvertTimeFromEpoch(epoch); x.String() != timeString {
+		t.Errorf("ConvertTimeFromEpoch(%v)=" + x.String() + ", want " + timeString, epoch)
+	}
+}
+
